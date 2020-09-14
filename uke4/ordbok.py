7d4e6f5aea933b2e8cd4b749e387b02712ef8f56
Andreas Limi <andreaslimidev@gmail.com>
2020-09-14 21:20:01 +0200

    uke 4 finalized

---
+++ uke4/ordbok.py
@@ -0,0 +1,16 @@
+beboere = {
+    "Andreas": ["frokost", "lunch", "middag"],
+    "Sigrunn": ["frokost", "lunch", "middag"],
+    "Turid": ["frokost", "lunch", "middag"]
+    }
+
+# print beboere
+
+
+# legg til en beboer
+
+
+# endre verdien til en nøkkel
+
+
+# sjekk om nøkkel finnes i ordbok
